6d7d05ace38ccf9f0af9486beed0621d811a1348
Connie Park <conniedc1206@gmail.com>
2022-06-07 13:41:40 -0700

    commit tree 6a991d91e38c600974dd15fff5635822b26d23b9

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,18 @@
 import React from "react";
 
-function ProjectItem({ name, about, technologies }) {
+function ProjectItem({ name, about, technologies}) {
+  //The ProjectItem component should get a prop of technologies passed down from ProjectList. For each element in this array of technologies, create a <span> tag that displays the name of the technology.
+  // 
+  const displayTech = technologies.map(technology => {
+    return <span key={technology}>{technology}</span>;
+  })
+  
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {displayTech}
       </div>
     </div>
   );
